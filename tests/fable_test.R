2aa0064c6fdfa5bc14a87b292fd50c1e1e10cd1a
David Holt <d@tdfd.org>
2020-01-18 11:19:34 -0500

    update fable_test.R: - added sourcing of data - made package calls explicit

--- tests/fable_test.R
+++ tests/fable_test.R
@@ -1,39 +1,43 @@
 devtools::load_all(".")
+library(tidyverse)
+library(fable)
 library(fabletools)
 library(future)
-library(fable.prophet)
 plan(multisession)
 
+source("data-raw/lax_passengers.R")
+data <- lax_passengers %>%
+  fill_gaps()
+
 data %>%
-  model(
-    naive = NAIVE(Count)
-    ,arima = ARIMA(Count)
-    ,prophet = prophet(Count ~ growth())
-    ,bsts_intercept = BSTS(Count ~ intercept(),
-                           iterations = 200)
-    ,bsts_autoar = BSTS(Count ~ ar("auto"),
+  fabletools::model(
+    naive = fable::NAIVE(passengers)
+    ,arima = fable::ARIMA(passengers)
+    # ,bsts_intercept = BSTS(passengers ~ intercept(),
+    #                        iterations = 200)
+    ,bsts_autoar = BSTS(passengers ~ ar("auto"),
                         iterations = 200)
-    ,bsts_ar = BSTS(Count ~ ar("specified", lags = 2),
-                    iterations = 200)
-    ,bsts_level = BSTS(Count ~ level(),
-                       iterations = 200)
-    ,bsts_local = BSTS(Count ~ trend("local"),
-                       iterations = 200)
-    ,bsts_semilocal = BSTS(Count ~ trend("semilocal"),
-                           iterations = 200)
-    ,bsts_student = BSTS(Count ~ trend("student"),
-                         iterations = 200)
-    ,bsts_int_autoar = BSTS(Count ~ intercept() + ar("auto"),
-                            iterations = 200)
-    ,bsts_seasonal = BSTS(Count ~ seasonal(period = "1 week"),
-                          iterations = 200)
-    ,bsts_seas_local = BSTS(Count ~ seasonal("1 week") + trend(),
-                            iterations = 200)
-    ,bsts_semi_seas = BSTS(Count ~ seasonal("1 week") + trend("semilocal"),
-                           iterations = 200)
-    ,bsts_trig = BSTS(Count ~ trig(period = "1 week", frequencies = 1),
-                      iterations = 200)
+    # ,bsts_ar = BSTS(passengers ~ ar("specified", lags = 2),
+    #                 iterations = 200)
+    # ,bsts_level = BSTS(passengers ~ level(),
+    #                    iterations = 200)
+    # ,bsts_local = BSTS(passengers ~ trend("local"),
+    #                    iterations = 200)
+    # ,bsts_semilocal = BSTS(passengers ~ trend("semilocal"),
+    #                        iterations = 200)
+    # ,bsts_student = BSTS(passengers ~ trend("student"),
+    #                      iterations = 200)
+    # ,bsts_int_autoar = BSTS(passengers ~ intercept() + ar("auto"),
+    #                         iterations = 200)
+    # ,bsts_seasonal = BSTS(passengers ~ seasonal(period = "1 week"),
+    #                       iterations = 200)
+    # ,bsts_seas_local = BSTS(passengers ~ seasonal("1 week") + trend(),
+    #                         iterations = 200)
+    # ,bsts_semi_seas = BSTS(passengers ~ seasonal("1 week") + trend("semilocal"),
+    #                        iterations = 200)
+    # ,bsts_trig = BSTS(passengers ~ trig(period = "1 week", frequencies = 1),
+    #                   iterations = 200)
   ) %>%
-  forecast(h = 50) %>%
-  autoplot(data = data[650:nrow(data),]) +
-  facet_wrap(~.model)
+  fabletools::forecast(h = 100) %>%
+  autoplot(data = data) +
+  facet_grid(rows = vars(.model))
