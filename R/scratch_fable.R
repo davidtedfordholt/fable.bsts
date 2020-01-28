557b0b21b22d89331d0f8432d6e80a7fd53e4de8
David Holt <d@tdfd.org>
2020-01-28 10:21:54 -0500

    update scratch_fable.R: - add mechanism to test for bug in AddMonthlyAnnualCycle

--- R/scratch_fable.R
+++ R/scratch_fable.R
@@ -1,39 +1,60 @@
-# devtools::load_all(".")
-library(tidyverse)
+library(dplyr)
 library(fable)
+library(tsibble)
+library(tsibbledata)
+library(ggplot2)
 source("./R/model.R")
 
 # future::plan(multisession)
 
-tictoc::tic()
-data <- tsibbledata::nyc_bikes %>%
-  tsibble::index_by(day = as.Date(start_time)) %>%
+.data <- nyc_bikes %>%
+  index_by(day = as.Date(start_time)) %>%
   # tsibble::index_by(month = tsibble::yearmonth(Time)) %>%
-  tsibble::group_by_key() %>%
-  dplyr::summarise(trips = n()) %>%
-  tsibble::fill_gaps(trips = 0)
+  group_by_key() %>%
+  summarise(trips = n()) %>%
+  fill_gaps(trips = 0)
 
-data %>%
-  fabletools::model(
-    naive = fable::NAIVE(trips)
-    ,snaive = fable::SNAIVE(trips)
-    ,arima = fable::ARIMA(trips)
-    # ,bsts_intercept = BSTS(trips ~ intercept(), iterations = 500)
-    # ,bsts_autoar = BSTS(trips ~ ar("auto"), iterations = 500)
-    # ,bsts_ar = BSTS(trips ~ ar("specified", lags = 2), iterations = 500)
-    # ,bsts_level = BSTS(trips ~ level(), iterations = 500)
-    # ,bsts_local = BSTS(trips ~ trend("local"), iterations = 500)
-    # ,bsts_semilocal = BSTS(trips ~ trend("semilocal"), iterations = 500)
-    # ,bsts_student = BSTS(trips ~ trend("student"), iterations = 500)
-    # ,bsts_int_autoar = BSTS(trips ~ intercept() + ar("auto"), iterations = 500)
-    # ,bsts_seasonal = BSTS(trips ~ level() + seasonal(period = "1 week"), iterations = 500)
-    # ,bsts_seas_local = BSTS(trips ~ seasonal("1 week") + trend(), iterations = 500)
-    # ,bsts_semi_seas = BSTS(trips ~ seasonal("1 week") + trend("semilocal"), iterations = 500)
-    # ,bsts_trig = BSTS(trips ~ level() + trig(period = "1 week"), iterations = 500)
-    # ,bsts_cycle = BSTS(trips ~ level() + cycle(), iterations = 500)
+plot(.data)
+autoplot(.data) + facet_wrap(.~ bike_id)
+
+iterations <- 100
+horizon <- 100
+
+.data %>%
+  as_tibble() %>%
+  group_by(bike_id) %>%
+  summarise(days = n()) %>%
+  arrange(days)
+
+mbl <-
+  .data %>%
+  # filter(bike_id != 33074) %>%
+  # filter(bike_id != 33074 && bike_id != 31735 && bike_id != 31681) %>%
+  model(
+    naive = NAIVE(trips)
+    # ,snaive = SNAIVE(trips)
+    # ,snaive_drift = SNAIVE(trips ~ drift())
+    # ,arima = ARIMA(trips)
+    # ,bsts_intercept = BSTS(trips ~ intercept(), iterations = iterations)
+    # ,bsts_autoar = BSTS(trips ~ ar("auto"), iterations = iterations)
+    # ,bsts_ar = BSTS(trips ~ ar("specified", lags = 2), iterations = iterations)
+    # ,bsts_level = BSTS(trips ~ level(), iterations = iterations)
+    # ,bsts_local = BSTS(trips ~ trend("local"), iterations = iterations)
+    # ,bsts_semilocal = BSTS(trips ~ trend("semilocal"), iterations = iterations)
+    # ,bsts_student = BSTS(trips ~ trend("student"), iterations = iterations)
+    # ,bsts_int_autoar = BSTS(trips ~ intercept() + ar("auto"), iterations = iterations)
+    # ,bsts_seasonal = BSTS(trips ~ level() + seasonal(period = "1 week"), iterations = iterations)
+    # ,bsts_seas_local = BSTS(trips ~ seasonal("1 week") + trend(), iterations = iterations)
+    # ,bsts_semi_seas = BSTS(trips ~ seasonal("1 week") + trend("semilocal"), iterations = iterations)
+    # ,bsts_trig = BSTS(trips ~ level() + trig(period = "1 week"), iterations = iterations)
+    ,bsts_cycle = BSTS(trips ~ cycle(), iterations = iterations)
     # ,bsts_broken = BSTS(trips ~ intercept() + ar() + level() + trend() + seasonal() + trig())
-  ) %>%
-  fabletools::forecast(h = 100) %>%
-  fabletools::autoplot(data = data) +
-  ggplot2::facet_grid(~ .model + bike_id)
-tictoc::toc()
+  )
+
+fbl <-
+  mbl %>%
+  forecast(h = horizon)
+
+fbl %>%
+  autoplot(data = .data) +
+  facet_grid(.model ~ bike_id)
