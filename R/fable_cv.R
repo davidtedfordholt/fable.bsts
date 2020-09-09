5cbe915e92ab0147340ba31fda4d20dc01ae1ad9
bradisbrad <bradhill119@gmail.com>
2020-09-08 22:47:55 -0500

    update R/fable_cv.R: - commented out tictoc calls

--- R/fable_cv.R
+++ R/fable_cv.R
@@ -5,9 +5,9 @@ rm(list = ls(all.names = TRUE))
 
 # PACKAGES =========================================================
 
-library(tictoc)
-tictoc::tic("total time")
-tictoc::tic("loading packages")
+# library(tictoc)
+# tictoc::tic("total time")
+# tictoc::tic("loading packages")
 library(tidyverse)
 library(tsibble)
 library(feasts)
@@ -16,7 +16,7 @@ library(fable.prophet)
 library(future)
 library(future.apply)
 future::plan("multisession")
-tictoc::toc()
+# tictoc::toc()
 
 
 # FUNCTIONS ========================================================
@@ -73,7 +73,7 @@ data <- tsibbledata::vic_elec %>%
 
 # MODELING =========================================================
 
-tictoc::tic("Modeling")
+# tictoc::tic("Modeling")
 object <-
   data %>%
   create_cutoff_dates(n_cv = 62, horizon = 14)
@@ -83,5 +83,5 @@ forecasts <-
   create_forecasts_for_all_cutoffs()
 
 
-tictoc::toc()
-tictoc::toc()
+# tictoc::toc()
+# tictoc::toc()
